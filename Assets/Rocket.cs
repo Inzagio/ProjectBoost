8853c44b92184b013b73a45a3b9acf6f5ac99717
Trym <chilled-1337@hotmail.com>
2018-07-14 01:26:05 +0200

    Rocket can now move some :) Hurray

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour {
-
+public class Rocket : MonoBehaviour
+{
+	Rigidbody rigidBody;
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		rigidBody = GetComponent<Rigidbody>();
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		ProcessInput();
+	}
+
+	private void ProcessInput()
+	{
+		// Apply thrust
+		if (Input.GetKey(KeyCode.Space))
+		{
+			rigidBody.AddRelativeForce(Vector3.up);
+		}
+		//Rotate Left
+		if (Input.GetKey(KeyCode.A))
+		{
+			//transform.Rotate((Vector3.forward) * Time.deltaTime);
+			transform.Rotate(Vector3.forward);
+		}
+		//Rotate right
+		else if (Input.GetKey(KeyCode.D))
+		{
+			//transform.Rotate((Vector3.back) * Time.deltaTime);
+			transform.Rotate(-Vector3.forward);
+		}
 	}
 }
