e3779625dc8fad7d913fa650c5a66a26d00e63b5
Trym <chilled-1337@hotmail.com>
2018-11-28 22:46:53 +0100

    Added SerializeField for rocketShip
    Added SerializeField for rotation and thrust, also added some downdraft

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,54 +1,68 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-	Rigidbody rigidBody;
+    Rigidbody _rigidBody;
+    AudioSource _thrustAudio;
+    [SerializeField] private float _rcsThrust = 100f;
+    [SerializeField] private float _engineThrust = 250f;
 
-	AudioSource thrustAudio;
-	// Use this for initialization
-	void Start()
-	{
-		rigidBody = GetComponent<Rigidbody>();
+    // Use this for initialization
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
 
-		thrustAudio = GetComponent<AudioSource>();
-	}
+        _thrustAudio = GetComponent<AudioSource>();
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-		ProcessInput();
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        Thrust();
+        Drag();
+    }
 
-	private void ProcessInput()
-	{
-		// Apply thrust
-		if (Input.GetKey(KeyCode.Space))
-		{
-			rigidBody.AddRelativeForce(Vector3.up);
-			if (!thrustAudio.isPlaying)
-			{
-				thrustAudio.Play();
-			}
-			
-		}
-		else
-		{
-			thrustAudio.Stop();
-		}
-		//Rotate Left
-		if (Input.GetKey(KeyCode.A))
-		{
-			//transform.Rotate((Vector3.forward) * Time.deltaTime);
-			transform.Rotate(Vector3.forward);
-		}
-		//Rotate right
-		else if (Input.GetKey(KeyCode.D))
-		{
-			//transform.Rotate((Vector3.back) * Time.deltaTime);
-			transform.Rotate(-Vector3.forward);
-		}
-	}
+    private void Rotate()
+    {
+        _rigidBody.freezeRotation = true;
+
+        float rotationThisFrame = _rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A)) //Left
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D)) //Right
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        _rigidBody.freezeRotation = false;
+    }
+
+    private void Thrust()
+    {
+        float thrustThisFrame = _engineThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+            if (!_thrustAudio.isPlaying)
+            {
+                _thrustAudio.Play();
+            }
+        }
+        else
+        {
+            _thrustAudio.Stop();
+        }
+    }
+    private void Drag()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            _rigidBody.AddRelativeForce(Vector3.down);
+        }
+    }
 }
