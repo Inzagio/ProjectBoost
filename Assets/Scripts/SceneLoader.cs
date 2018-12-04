5f5cf5998b5ab749cf45d760fd04b4d59609715e
Trym <chilled-1337@hotmail.com>
2018-12-04 23:00:15 +0100

    Added some more levels

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        var currentScene = SceneManager.GetActiveScene().name;
+
+        if (currentScene == "Start Menu")
+        {
+            SceneManager.LoadScene("Core Game");
+        }
+
+        else if (currentScene == "Core Game")
+        {
+            SceneManager.LoadScene("Win Screen");
+        }
+
+        else if (currentScene == "Win Screen")
+        {
+            SceneManager.LoadScene("Start Menu");
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadNextSceneAlternative()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex == 0)
+        {
+            SceneManager.LoadScene("Core Game");
+        }
+        else if (currentSceneIndex == 1)
+        {
+            SceneManager.LoadScene("Win Screen");
+        }
+        else if (currentSceneIndex == 2)
+        {
+            SceneManager.LoadScene("Start Menu");
+        }
+    }
+}
